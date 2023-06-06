856f80c83c373147dc33de4e72e5af9d157fb4d2
JonnathanMTalent <jonnathan.monroy741@gmail.com>
2023-06-06 13:50:06 -0500

    localStorage y Responsive design

--- jhonMircha/Proyectos/menu_de_Amburguesa/js/dom/tema_oscuro.js
+++ jhonMircha/Proyectos/menu_de_Amburguesa/js/dom/tema_oscuro.js
@@ -1,4 +1,4 @@
-let d=document;
+let d=document, ls=localStorage;
 
 export default function darkTheme(btn, classDark){ // classDark esta en dom_ejercicios.css y es dark-theme, es el que cambia todo el estilo
     const $themeBtn=d.querySelector(btn),
@@ -7,17 +7,42 @@ export default function darkTheme(btn, classDark){ // classDark esta en dom_ejer
     let moon="🌙",
     sun="☀️";
 
+    const lightMode=()=>{
+        $selectors.forEach(el=> el.classList.remove(classDark));
+        $themeBtn.textContent=moon;
+        ls.setItem("theme","light");
+    };
+    
+    const darkMode=()=>{
+        $selectors.forEach(el=> el.classList.add(classDark));
+        $themeBtn.textContent=sun;
+        ls.setItem("theme","dark");
+    }
     //Este listener cambia la clase a la contraria detectando si esta puesto el texto de la luna o del sol.
     d.addEventListener("click",(e)=>{
         if(e.target.matches(btn)){
             console.log($themeBtn.textContent);
             if($themeBtn.textContent === moon){
-                $selectors.forEach(el=> el.classList.add(classDark));
-                $themeBtn.textContent=sun;
+                // $selectors.forEach(el=> el.classList.add(classDark));
+                // $themeBtn.textContent=sun;
+                darkMode();
+
             }else{
-                $selectors.forEach(el=> el.classList.remove(classDark));
-                $themeBtn.textContent=moon;
+                // $selectors.forEach(el=> el.classList.remove(classDark));
+                // $themeBtn.textContent=moon;
+                lightMode();
             }
         }
     });
+
+    //LOCAL STORAGE = Ls
+    // SE PUEDE VER EL VALOR DE LAS VARIABLES GUARDADAS EN LA CONSOLA EN LA OPCION APLICATION
+    d.addEventListener("DOMContentLoaded",(e)=>{
+        // alert("Hola desde la funcion darktheme");
+        // console.log(ls.getItem("theme"));
+        if(ls.getItem("theme")===null)ls.setItem("theme","light");
+        if(ls.getItem("theme")==="light")lightMode();
+        if(ls.getItem("theme")==="dark")darkMode();
+    });
+
 }
--- jhonMircha/Proyectos/menu_de_Amburguesa/js/index_dom.js
+++ jhonMircha/Proyectos/menu_de_Amburguesa/js/index_dom.js
@@ -4,6 +4,7 @@ import { moveBall,shortcuts } from "./dom/teclado.js";
 import countdown from "./dom/cuenta_regresiva.js";
 import scrollTopButton from "./dom/boton_scroll.js";
 import darkTheme from "./dom/tema_oscuro.js";
+import responsiveMedia from "./dom/objeto_responsive.js";
 
 const d=document;
 
@@ -13,7 +14,19 @@ d.addEventListener("DOMContentLoaded",(e)=>{ // esto permite que cargue el dom a
     alarm("../../assets/Comodo.mp3","#activar-alarma","#desactivar-alarma");
     countdown("countdown","May 23, 2023 03:23:19","Feliz cumpleaños! 🤓"); //no fue necesario poner el # que indica el id en el selector porque se llamo con un getid y no con un querySelector.
     scrollTopButton(".scroll-top-btn");
-    darkTheme(".dark-theme-btn","dark-mode");
+    // darkTheme(".dark-theme-btn","dark-mode"); // se quito de aqui y se puso mas abajo afuera del DOMContentLoaded, porque se llamo otro evento DOMContentLoaded para usar el localStorage
+    responsiveMedia(
+    "youtuve",
+    "(min-width: 1024px)",
+    '<a href="https://www.youtube.com/watch?v=6IwUl-4pAzc&list=PLvq-jIkSeTUZ6QgYYO3MwG9EMqC-KoLXA&index=92">ver video en youtube</a>',
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/6IwUl-4pAzc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    );
+    responsiveMedia(
+    "gmaps",
+    "(min-width: 1024px)",
+    '<a href="https://goo.gl/maps/G9nWc6eDAhNZ1J1J7">ver Mapa</a>',
+    '<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d63451.50444539488!2d-75.61214203670626!3d6.300602209631392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smapa%20universidad%20de%20antioquia!5e0!3m2!1ses!2sco!4v1686072202848!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+    );
 })
 //https://emojipedia.org/es/  AQUI PUEDO SACAR EMOTICONES POR BÚSQUEDA.
 // Con este código vamos a detectar los shortcuts que se tecleen cuando este posado sobre el viewport (Interfaz de usuario)
@@ -23,3 +36,5 @@ d.addEventListener("keydown",(e)=>{ //El evento keydown es cuando se teclea una 
     shortcuts(e);
     moveBall(e,".ball",".stage");
 });
+
+darkTheme(".dark-theme-btn","dark-mode"); // se quito de arriba y se puso aqui afuera del DOMContentLoaded, porque se llamo otro evento DOMContentLoaded para usar el localStorage
